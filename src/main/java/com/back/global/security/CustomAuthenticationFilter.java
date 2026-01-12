d9297a5d4ae3853fdf928741fd8de6b09f527443
kimdohwan7220 <dohwan8220@naver.com>
2026-01-12 14:33:37 +0900

    feat: 수동 JSON화 작업을 잭슨(objectMapper)을 이용해서 처리, objectMapper 주입받기

--- src/main/java/com/back/global/security/CustomAuthenticationFilter.java
+++ src/main/java/com/back/global/security/CustomAuthenticationFilter.java
@@ -5,6 +5,7 @@ import com.back.domain.member.member.service.MemberService;
 import com.back.global.exception.ServiceException;
 import com.back.global.rq.Rq;
 import com.back.global.rsData.RsData;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -29,6 +30,7 @@ import java.util.Map;
 public class CustomAuthenticationFilter extends OncePerRequestFilter {
     private final MemberService memberService;
     private final Rq rq;
+    private final ObjectMapper objectMapper;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -40,12 +42,9 @@ public class CustomAuthenticationFilter extends OncePerRequestFilter {
             RsData<Void> rsData = e.getRsData();
             response.setContentType("application/json");
             response.setStatus(rsData.statusCode());
-            response.getWriter().write("""
-                    {
-                        "resultCode": "%s",
-                        "msg": "%s"
-                    }
-                    """.formatted(rsData.resultCode(), rsData.msg()));
+            response.getWriter().write(
+                    objectMapper.writeValueAsString(rsData)
+            );
         } catch (Exception e) {
             throw e;
         }
