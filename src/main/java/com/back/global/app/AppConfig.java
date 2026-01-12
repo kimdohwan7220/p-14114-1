da50de119a6dda756ebc108e3522d38e32e17207
kimdohwan7220 <dohwan8220@naver.com>
2026-01-12 15:21:10 +0900

    feat: Ut가 AppConfig을 모르는 구조

--- src/main/java/com/back/global/app/AppConfig.java
+++ src/main/java/com/back/global/app/AppConfig.java
@@ -1,5 +1,8 @@
 package com.back.global.app;
 
+import com.back.standard.util.Ut;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -35,5 +38,17 @@ public class AppConfig {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    private static ObjectMapper objectMapper;
+
+    @Autowired
+    public void setObjectMapper(ObjectMapper objectMapper) {
+        AppConfig.objectMapper = objectMapper;
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        Ut.json.objectMapper = objectMapper;
     }
 }
--- src/main/java/com/back/standard/util/Ut.java
+++ src/main/java/com/back/standard/util/Ut.java
@@ -70,7 +70,7 @@ public class Ut {
     }
 
     public static class json {
-        private static final ObjectMapper objectMapper = new ObjectMapper();
+        public static ObjectMapper objectMapper;
 
         public static String toString(Object object) {
             return toString(object, null);
